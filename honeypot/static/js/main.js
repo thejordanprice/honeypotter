2beb2ae492ce53c270c28e95f1617066ddfc3ef0
darcsyntax <darcsyntax@gmail.com>
2025-03-27 07:36:29 -0400

    modified:   honeypot/static/js/main.js

--- honeypot/static/js/main.js
+++ honeypot/static/js/main.js
@@ -3386,20 +3386,3 @@ function initializeMap() {
 function shouldUseAnimationTimeouts() {
     return window.animationMode > 0 && window.animationsEnabled;
 }
-
-// Workaround for 1px lines appearing in some browsers due to fractional transforms
-// and resulting anti-aliasing.
-// https://github.com/Leaflet/Leaflet/issues/3575
-(function(){
-    var originalInitTile = L.GridLayer.prototype._initTile
-    L.GridLayer.include({
-        _initTile: function (tile) {
-            originalInitTile.call(this, tile);
-
-            var tileSize = this.getTileSize();
-
-            tile.style.width = tileSize.x + 1 + 'px';
-            tile.style.height = tileSize.y + 1 + 'px';
-        }
-    });
-})();
